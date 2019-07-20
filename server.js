0af3ea9c319c41cec6db1f95e588c9e7f72752ab
alexisjcarr <acarr59@gmail.com>
2019-07-20 17:02:39 -0500

    ➕ Adds boilerplate and simple server.
     #1

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const server = express();
+
+server.use(helmet());
+server.use(bodyParser.json);
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    kill: "yourself"
+  });
+});
+
+module.exports = server;
