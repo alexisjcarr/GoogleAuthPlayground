a4b18cfe7ede306ffc84f10539697da5698f1e58
alexisjcarr <acarr59@gmail.com>
2019-07-20 17:17:17 -0500

    🐛 Fixes bug by changing bodyParser.json to bodyParser.json().
     #1

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const bodyParser = require("body-parser");
 const server = express();
 
 server.use(helmet());
-server.use(bodyParser.json);
+server.use(bodyParser.json());
 server.use(cors());
 
 server.get("/", (req, res) => {
-  res.status(200).json({
-    please: "work"
-  });
+  res.send("<h1>The Playground Is Open</h1>");
 });
 
 module.exports = server;
